61c6ad5454f4737d1059b5369867688528e8494b
GianiHeits <giani.statie@heits.digital>
2022-05-10 13:20:20 +0300

    Final round

--- fraction.js
+++ fraction.js
@@ -1,14 +1,32 @@
 module.exports = class Fraction {
-    constructor(numerator, denominator=1) {
-        this.numerator = numerator;
-        this.denominator = denominator;
+    constructor(numerator, denominator = 1) {
+      this.numerator = numerator;
+      this.denominator = denominator;
     }
- 
     getIntegerValue() {
-        return this.numerator;
+      return this.numerator;
     }
-
+  
     add(fraction) {
-        return this.getIntegerValue() + fraction.getIntegerValue();
+      if (this.hasSameDenominator(fraction)) {
+        const numertorSum = this.getIntegerValue() + fraction.getIntegerValue();
+        return new Fraction(numertorSum, this.denominator);
+      } else {
+        const commonNumerator =
+          this.numerator * fraction.denominator +
+          fraction.numerator * this.denominator;
+        const commonDenominator = this.denominator * fraction.denominator;
+        return new Fraction(commonNumerator, commonDenominator);
+      }
     }
- }
+    getNumerator() {
+      return this.numerator;
+    }
+    getDenominator() {
+      return this.denominator;
+    }
+  
+    hasSameDenominator(fraction) {
+      return this.denominator == fraction.denominator;
+    }
+  };
--- fraction.test.js
+++ fraction.test.js
@@ -1,10 +1,25 @@
-var Fraction = require('./fraction.js');
-
-test('verifies value fetching', () => {
-    expect(new Fraction(0).getIntegerValue()).toBe(0);
+var Fraction = require("./fraction.js");
+test("verifies value fetching", () => {
+  expect(new Fraction(0).getIntegerValue()).toBe(0);
 });
 
-test('verifies addition without denominator', () => {
-    expect(new Fraction(0).add(new Fraction(0))).toBe(0);
-    expect(new Fraction(3).add(new Fraction(0))).toBe(3);
+test("verifies addition without denominator", () => {
+  expect(new Fraction(0).add(new Fraction(0)).getIntegerValue()).toBe(0);
+  expect(new Fraction(3).add(new Fraction(0)).getIntegerValue()).toBe(3);
+  expect(new Fraction(0).add(new Fraction(5)).getIntegerValue()).toBe(5);
+});
+
+test("verifies 1/5+2/5=3/5", () => {
+  expect(new Fraction(1, 5).add(new Fraction(2, 5)).getNumerator()).toBe(3);
+  expect(new Fraction(1, 5).add(new Fraction(2, 5)).getDenominator()).toBe(5);
+});
+
+test("verifies 1/6+2/6=3/6", () => {
+  expect(new Fraction(1, 6).add(new Fraction(2, 6)).getNumerator()).toBe(3);
+  expect(new Fraction(1, 6).add(new Fraction(2, 6)).getDenominator()).toBe(6);
+});
+
+test("verifies 1/2+1/3=5/6", () => {
+  expect(new Fraction(1, 2).add(new Fraction(1, 3)).getNumerator()).toBe(5);
+  expect(new Fraction(1, 2).add(new Fraction(1, 3)).getDenominator()).toBe(6);
 });
